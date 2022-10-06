d85321aefe6dcc4b24910fca78cb33f072a41b53
Codex404 <git@codexnotfound.nl>
2022-10-06 20:57:38 +0200

    Implement collapse exclusion list

--- src/main/java/nl/codexnotfound/tweaks_not_found/mixin/ChatMixin.java
+++ src/main/java/nl/codexnotfound/tweaks_not_found/mixin/ChatMixin.java
@@ -31,32 +31,45 @@ public class ChatMixin {
     private final Pattern COLLAPSE_CHAT_FORMAT_REGEX = Pattern.compile(" \\{×(\\d+)}");
     private final String TIMESTAMP_FORMAT = "[%02d:%02d] ";
     private final Pattern TIMESTAMP_FORMAT_REGEX = Pattern.compile("\\[\\d{2}:\\d{2}] ");
-    @ModifyArgs(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At(value = "INVOKE", target="Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;I)V"))
-    private void modifyArgsAddMessage(org.spongepowered.asm.mixin.injection.invoke.arg.Args args){
+
+    private static final List<String> nonCollapsingMessages = List.of(
+            "\\o",
+            "o/"
+    );
+
+    @ModifyArgs(method = "addMessage(Lnet/minecraft/text/Text;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;I)V"))
+    private void modifyArgsAddMessage(org.spongepowered.asm.mixin.injection.invoke.arg.Args args) {
         MutableText msg = args.get(0);
         var style = msg.getStyle();
         var timeText = buildTimePrefix();
         msg = timeText.append(msg.setStyle(style));
         args.set(0, msg);
     }
+
     @Inject(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At("HEAD"), cancellable = true)
-    private void onAddMessage(Text msg, int messageId, int timestamp, boolean refresh, CallbackInfo ci){
-        var message = (MutableText)msg;
+    private void onAddMessage(Text msg, int messageId, int timestamp, boolean refresh, CallbackInfo ci) {
+        var message = (MutableText) msg;
         var searchDistance = Math.min(COLLAPSE_CHAT_SEARCH_DISTANCE, messages.size());
         var newMessageText = message.getString();
+        var newMessageTextCleanedUp = newMessageText.replaceFirst(TIMESTAMP_FORMAT_REGEX.pattern(), "").trim();
+
+        if (nonCollapsingMessages.stream().anyMatch(newMessageTextCleanedUp::contains)) {
+            return;
+        }
+
         var sameMessageIndex = -1;
         // See if the message has been send before
-        for(int i=0; i<searchDistance; i++){
+        for (int i = 0; i < searchDistance; i++) {
             var tempMessage = messages.get(i).getText().getString()
                     .replaceFirst(TIMESTAMP_FORMAT_REGEX.pattern(), "")
                     .replaceAll(COLLAPSE_CHAT_FORMAT_REGEX.pattern(), "");
 
-            if(tempMessage.equals(newMessageText.replaceFirst(TIMESTAMP_FORMAT_REGEX.pattern(), ""))) {
+            if (tempMessage.equals(newMessageTextCleanedUp)) {
                 sameMessageIndex = i;
                 break;
             }
         }
-        if(sameMessageIndex == -1){
+        if (sameMessageIndex == -1) {
             return;
         }
 
@@ -64,7 +77,7 @@ public class ChatMixin {
         var oldMessage = messages.get(sameMessageIndex);
         var match = COLLAPSE_CHAT_FORMAT_REGEX.matcher(oldMessage.getText().getString());
         var count = 2;
-        if(match.find()) {
+        if (match.find()) {
             count = Integer.parseInt(match.group(1)) + 1;
         }
 
@@ -81,9 +94,9 @@ public class ChatMixin {
         List<OrderedText> lines = ChatMessages.breakRenderedChatMessageLines(newChatText, width, this.client.textRenderer);
 
         int sameVisibleMessageIndex = sameMessageIndex; // Use as fallback
-        for(int i=0; i<this.visibleMessages.size(); i++){
+        for (int i = 0; i < this.visibleMessages.size(); i++) {
             var visibleMessage = this.visibleMessages.get(i);
-            if(visibleMessage.getCreationTick() == oldMessage.getCreationTick()){
+            if (visibleMessage.getCreationTick() == oldMessage.getCreationTick()) {
                 sameVisibleMessageIndex = i;
                 break;
             }
@@ -91,14 +104,14 @@ public class ChatMixin {
 
         for (int i = 0; i < lines.size(); i++) {
             OrderedText orderedText = lines.get(i);
-            var absoluteLineIndex = sameVisibleMessageIndex+i;
+            var absoluteLineIndex = sameVisibleMessageIndex + i;
             this.visibleMessages.remove(absoluteLineIndex);
             this.visibleMessages.add(0, new ChatHudLine(timestamp, orderedText, messageId));
         }
         ci.cancel();
     }
 
-    private MutableText buildTimePrefix(){
+    private MutableText buildTimePrefix() {
         var time = LocalTime.now();
         var hours = time.getHour();
         var minutes = time.getMinute();
@@ -108,7 +121,7 @@ public class ChatMixin {
 //                .setStyle(Style.EMPTY.withColor(TextColor.parse("aqua")));
     }
 
-    private double getWidth(){
+    private double getWidth() {
         return MathHelper.floor(client.options.getChatWidth().getValue() * 280.0 + 40.0);
     }
 
